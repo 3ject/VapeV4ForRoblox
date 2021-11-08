46efc2cb1e7f5169c062cb3ab8b9d4d466ba5c20
3ject <88593579+3ject@users.noreply.github.com>
2021-11-07 20:54:07 -0500

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -4,7 +4,7 @@ local function GetURL(scripturl)
 	if shared.VapeDeveloper then
 		return readfile("vape/"..scripturl)
 	else
-		return game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/"..scripturl, true)
+		return game:HttpGet("https://raw.githubusercontent.com/3ject/VapeV4ForRoblox/main/"..scripturl, true)
 	end
 end
 local getasset = getsynasset or getcustomasset
